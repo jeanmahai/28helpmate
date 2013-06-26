b86754e7c69694e4611beed2976ee0dae3a2eb29
nick <zhengyanming2006@163.com>
2013-06-27 01:35:30 +0800

    create data service projects.

---
+++ src/DataService/01_Service/Helpmate.DataService.Beijing/CollectionSvc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace Helpmate.DataService.Beijing
+{
+    public partial class CollectionSvc : ServiceBase
+    {
+        public CollectionSvc()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
