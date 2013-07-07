7dc86f78d3ac130bd6335e18182e23a5774293f6
nick <zhengyanming2006@163.com>
2013-07-08 01:43:11 +0800

    add files.

---
+++ src/SoftWare/01_Portal/02_BizFocade/Helpmate.Facades/TrendFacade.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Helpmate.Facades.LotteryWebService;
+
+namespace Helpmate.Facades
+{
+    public class TrendFacade
+    {
+        private readonly RestClient restClient = new RestClient();
+
+        public void QueryTrend(int pageIndex, Action<QueryCompletedEventArgs> callback)
+        {
+            restClient.ClientService.QueryTrendAsync(DateTime.Now.AddDays(-1), DateTime.Now, 1000, pageIndex);
+            EventHandler<QueryCompletedEventArgs> callbackHandler = (obj, args) =>
+            {
+                callback(args);
+            };
+            restClient.ClientService.QueryCompleted += callbackHandler;
+        }
+        public LotteryTrend QueryTrend(int pageIndex)
+        {
+            LotteryWebServiceSoapClient svc = new LotteryWebServiceSoapClient("LotteryWebServiceSoap");
+            var data = svc.QueryTrend(DateTime.Now.AddDays(-1), DateTime.Now, 100, pageIndex);
+            return data;
+        }
+    }
+}
