ff3a32f655a6f6e009dccf27bf472321a0af8491
johnnyjiang <Johnny@Jiang.com>
2013-07-11 20:42:20 +0800

    Change Code

--- src/SoftWare/01_Portal/UI/Default.Designer.cs
+++ src/SoftWare/01_Portal/UI/Default.Designer.cs
@@ -687,8 +687,9 @@
             this.IsMdiContainer = true;
             this.MinimumSize = new System.Drawing.Size(1024, 600);
             this.Name = "Default";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "28伴侣-分析平台";
-            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Default_Load);
             this.stsPage.ResumeLayout(false);
             this.stsPage.PerformLayout();
--- src/SoftWare/01_Portal/UI/LoginForm.cs
+++ src/SoftWare/01_Portal/UI/LoginForm.cs
@@ -11,82 +11,99 @@ using Helpmate.BizEntity.Enum;
 using Utility.Update;
 using Common.Utility;
 using System.Diagnostics;
+using Helpmate.Facades.LotteryWebService;
 
 namespace Helpmate.UI.Forms
 {
     public partial class LoginForm : Form
     {
-        private string dbAddress = string.Empty;
-        private string dbName = string.Empty;
-        private string userName = string.Empty;
-        private string userPwd = string.Empty;
-
         public LoginForm()
         {
             InitializeComponent();
         }
 
+        public User user = new User();
+
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            //btnLogin.Enabled = false;
-            //dbAddress = txtSqlAddress.Text.Trim();
-            //dbName = txtDBName.Text.Trim();
-            //userName = txtUserName.Text.Trim();
-            //userPwd = txtUserPwd.Text.Trim();
+            user.UserName = txtUserName.Text.Trim();
+            user.UserPwd = txtUserPwd.Text;
 
-            //pnlLoading.Controls.Clear();
+            pnlLoading.Controls.Clear();
 
-            //string msg = ValidationTool.IsEmpty(dbAddress, "Sql连接地址");
-            //if (!string.IsNullOrEmpty(msg)) { pnlLoading.Controls.Add(LoadingCtrl.LoadModel(AppMessage.Error, msg)); txtSqlAddress.Focus(); return; }
+            string msg = ValidationTool.IsEmpty(user.UserName, "用户名");
+            if (!string.IsNullOrEmpty(msg)) { pnlLoading.Controls.Add(LoadingCtrl.LoadModel(MessageType.Error, msg)); txtUserName.Focus(); return; }
 
-            //msg = ValidationTool.IsEmpty(dbName, "数据库名称");
-            //if (!string.IsNullOrEmpty(msg)) { pnlLoading.Controls.Add(LoadingCtrl.LoadModel(AppMessage.Error, msg)); txtDBName.Focus(); return; }
+            msg = ValidationTool.IsEmpty(user.UserPwd, "密码");
+            if (!string.IsNullOrEmpty(msg)) { pnlLoading.Controls.Add(LoadingCtrl.LoadModel(MessageType.Error, msg)); txtUserPwd.Focus(); return; }
 
-            //msg = ValidationTool.IsEmpty(userName, "用户名");
-            //if (!string.IsNullOrEmpty(msg)) { pnlLoading.Controls.Add(LoadingCtrl.LoadModel(AppMessage.Error, msg)); txtUserName.Focus(); return; }
-
-            //msg = ValidationTool.IsEmpty(userPwd, "密码");
-            //if (!string.IsNullOrEmpty(msg)) { pnlLoading.Controls.Add(LoadingCtrl.LoadModel(AppMessage.Error, msg)); txtUserPwd.Focus(); return; }
-
+            btnLogin.Enabled = false;
             pnlLoading.Controls.Add(LoadingCtrl.LoadModel());
+
             bgwUserLogin.RunWorkerAsync();
+            //bgwUpdate.RunWorkerAsync();
         }
 
-        private void bgwUserLogin_DoWork(object sender, DoWorkEventArgs e)
+        private void backgroundUpdate_DoWork(object sender, DoWorkEventArgs e)
         {
             UpdateClass updater = new UpdateClass();
             var list = updater.CheckForUpdates();
             e.Result = list.Length > 0;
         }
 
-        private void bgwUserLogin_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        private void backgroundUpdate_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
+            btnLogin.Enabled = true;
+            pnlLoading.Controls.Clear();
+
             if (e.Error != null)
             {
-                AppMessage.AlertMessage("请检查网络连接是否正常，客户端检测更新失败！");
-                this.Close();
+                AppMessage.AlertMessage("请检查网络是否正常，稍后再试！");
+                return;
             }
-            else if (Convert.ToBoolean(e.Result))
+
+            if (Convert.ToBoolean(e.Result))
             {
                 UtilsTool.Alert("发现新的版本，立即更新！");
                 Process.Start("Update.exe", "121");
             }
-
-            btnLogin.Enabled = true;
-            if (Convert.ToBoolean(e.Result))
-            {
-                pnlLoading.Controls.Add(LoadingCtrl.LoadModel(MessageType.Right, "Sql连接成功！"));
-                this.DialogResult = DialogResult.OK;
-            }
             else
             {
-                pnlLoading.Controls.Add(LoadingCtrl.LoadModel(MessageType.Error, "请检查输入信息,Sql连接错误！"));
+                bgwUserLogin.RunWorkerAsync();
             }
         }
 
-        private void LoginForm_Load(object sender, EventArgs e)
+        private void bgwUserLogin_DoWork(object sender, DoWorkEventArgs e)
         {
+            if (user.UserName == "admin" && user.UserPwd == "admin")
+            {
+                e.Result = 800;
+            }
+            else
+            {
+                e.Result = 400;
+            }
+        }
 
+        private void bgwUserLogin_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            btnLogin.Enabled = true;
+            if (e.Error != null)
+            {
+                pnlLoading.Controls.Add(LoadingCtrl.LoadModel(MessageType.Error, "用户登录失败，请稍后再试！"));
+                return;
+            }
+
+            switch (Convert.ToInt32(e.Result))
+            {
+                case 800:
+                    this.DialogResult = DialogResult.OK;
+                    break;
+
+                case 400:
+                    pnlLoading.Controls.Add(LoadingCtrl.LoadModel(MessageType.Error, "用户或密码错误！"));
+                    break;
+            }
         }
     }
 }
