8683ae1738aaf67406e26bf18ffb249d7e199ba9
nick <zhengyanming2006@163.com>
2013-07-14 17:09:16 +0800

    modify code.

---
+++ src/SoftWare/01_Portal/QueryFilter/SuperTrendFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Helpmate.QueryFilter
+{
+    public class SuperTrendFilter
+    {
+        public int PageIndex { get; set; }
+        public string Date { get; set; }
+        public string Hour { get; set; }
+        public string Minute { get; set; }
+    }
+}
