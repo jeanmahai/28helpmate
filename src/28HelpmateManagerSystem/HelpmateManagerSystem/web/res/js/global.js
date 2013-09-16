cfa60f24667d62b268663f20325ef0d4a4297e19
jeanmahai <mahai_1986@126.com>
2013-09-16 20:23:33 +0800

    11

---
+++ src/28HelpmateManagerSystem/HelpmateManagerSystem/web/res/js/global.js
@@ -0,0 +1,16 @@
+﻿
+
+(function () {
+    var footer = $("div[data-role=footer]");
+    var nav = $("div[data-role=navigation]");
+    var cnt = $("div[data-role=content]");
+    var win = $(window);
+
+    function resetFooter() {
+        var winHeight = win.height();
+        var bodyHeight = $(document.body).height();
+        if(bodyHeight<winHeight) {
+            
+        }
+    }
+})();
