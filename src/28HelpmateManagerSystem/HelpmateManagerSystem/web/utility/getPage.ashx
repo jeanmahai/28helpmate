88bbb27038aa8a2e0dceca73e5c44415428701eb
jean-home <mahai_1986@126.com>
2013-09-21 15:43:06 +0800

    login

---
+++ src/28HelpmateManagerSystem/HelpmateManagerSystem/web/utility/getPage.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="getPage.ashx.cs" Class="web.utility.getPage" %>
---
+++ src/28HelpmateManagerSystem/HelpmateManagerSystem/web/utility/getPage.ashx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.IO;
+
+namespace web.utility
+{
+    /// <summary>
+    /// getPage 的摘要说明
+    /// </summary>
+    public class getPage:HttpHandlerBase
+    {
+        public override void DoRequest(HttpContext ctx)
+        {
+            var pageName = ctx.Request.QueryString["pageName"];
+            ctx.Response.Write(FindPage(pageName));
+        }
+
+        private string FindPage(string pageName)
+        {
+            var path = AppDomain.CurrentDomain.BaseDirectory;
+            path = Path.Combine(path,"res","pages",pageName + ".htm");
+            if (File.Exists(path))
+            {
+                using (var reader = new StreamReader(path,Encoding.UTF8))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+            return "";
+        }
+    }
+}
